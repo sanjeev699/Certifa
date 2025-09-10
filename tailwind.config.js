9e6df536c25ceb2b46a7bedca3e55f14bf9d7b4a
sanjeev699 <bhayya007@gmail.com>
2025-09-11 02:38:55 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        certifaBlue: "#1E3A8A",
+        certifaYellow: "#FACC15"
+      }
+    },
+  },
+  plugins: [],
+}
